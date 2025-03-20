a5ceff5fbcc7b2a1bf698764521d183fed60d001
DongThan <thantrongdong1144@gmail.com>
2025-03-20 14:16:38 +0700

    tmp

--- main.ts
+++ main.ts
@@ -3,8 +3,18 @@ import * as OBC from "@thatopen-platform/components-beta";
 import Stats from "stats.js";
 import { cameraUpdate } from "./libs/camera-controls.helper";
 
+const SELCTION_MODE = {
+  DEFAULT: "DEFAULT",
+  FUSTUM: "FUSTUM",
+}
+
+let seclectionMode = SELCTION_MODE.DEFAULT
+
 async function main() {
   // Set up scene
+  let frustumStartPoint = {clientX: 0, clientY: 0}
+  let frustumEndPoint = {clientX: 0, clientY: 0}
+  let isStartDrawFrustum = false
 
   const components = new OBC.Components();
   const worlds = components.get(OBC.Worlds);
@@ -97,31 +107,102 @@ async function main() {
   world.camera.controls['update'] = cameraUpdate
   world.camera.controls.minDistance = 0.01
 
+  let frustumRectangle = document.getElementById("frustumRectangle") as HTMLDivElement;
+  const updateFrustumRectangle = () => {
+    if(!isStartDrawFrustum) return
+    const x = Math.min(frustumStartPoint.clientX, frustumEndPoint.clientX)
+    const y = Math.min(frustumStartPoint.clientY, frustumEndPoint.clientY)
+    const width = Math.abs(frustumEndPoint.clientX - frustumStartPoint.clientX)
+    const height = Math.abs(frustumEndPoint.clientY - frustumStartPoint.clientY)
+    frustumRectangle.style.display = "block"
+    frustumRectangle.style.left = x + "px"
+    frustumRectangle.style.top = y + "px"
+    frustumRectangle.style.width = width + "px"
+    frustumRectangle.style.height = height + "px"
+  }
+  const hideFrustumRectangle = () => {
+    frustumRectangle.style.display = "none"
+  }
+
   // Create plane on click
   container.onpointerdown = async (event) => {
-    const result = await caster.castRay();
-    hidePivotPoint()
-    if(result && result.point && event.button === 0) {
-      const widthHalf = container.clientWidth / 2;
-      const heightHalf = container.clientHeight / 2;
-
-      world.camera.controls.setOrbitPoint(result.point.x, result.point.y, result.point.z);
-      
-      const projectPoint = result.point.clone().project(world.camera.three);
-      
-      let x = projectPoint.x * widthHalf + widthHalf
-      let y = - (projectPoint.y * heightHalf) + heightHalf
-      showPivotPoint(x, y)
+    if(event.target && event.target['tagName'] !== "CANVAS") return
+    if(seclectionMode == SELCTION_MODE.DEFAULT) {
+      const result = await caster.castRay();
+      hidePivotPoint()
+      if(result && result.point && event.button === 0) {
+        const widthHalf = container.clientWidth / 2;
+        const heightHalf = container.clientHeight / 2;
+  
+        world.camera.controls.setOrbitPoint(result.point.x, result.point.y, result.point.z);
+        
+        const projectPoint = result.point.clone().project(world.camera.three);
+        
+        let x = projectPoint.x * widthHalf + widthHalf
+        let y = - (projectPoint.y * heightHalf) + heightHalf
+        showPivotPoint(x, y)
+      }
+    } else if(seclectionMode == SELCTION_MODE.FUSTUM) {
+      isStartDrawFrustum = true
+      frustumStartPoint = {clientX: event.clientX, clientY: event.clientY}
     }
   };
 
-  container.onpointerup = () => {
-    hidePivotPoint()
+  container.onpointermove = (event) => {
+    if(seclectionMode == SELCTION_MODE.FUSTUM && isStartDrawFrustum) {
+      frustumEndPoint = {clientX: event.clientX, clientY: event.clientY}
+      updateFrustumRectangle()
+    }
+    if(event.target && event.target['tagName'] !== "CANVAS") return
   }
 
-  container.onwheel = () => {
+  container.onpointerup = (event) => {
+    isStartDrawFrustum = false
+    // onChangeMode(SELCTION_MODE.DEFAULT, null)
+    hideFrustumRectangle()
     hidePivotPoint()
+    if(event.target && event.target['tagName'] !== "CANVAS") return
   }
+
+  container.onwheel = (event) => {
+    isStartDrawFrustum = false
+    onChangeMode(SELCTION_MODE.DEFAULT, null)
+    hidePivotPoint()
+    if(event.target && event.target['tagName'] !== "CANVAS") return
+  }
+
+  const disableControl = () => {
+    world.camera.controls.enabled = false
+  }
+
+  const enableControl = () => {
+    world.camera.controls.enabled = true
+  }
+
+  const fustumModeButton = document.getElementById("frustumMode") as HTMLButtonElement;
+
+  const allSelectionModeButtons = [fustumModeButton]
+
+  const onChangeMode = (newMode, htmlButton) => {
+    if(seclectionMode === newMode || newMode === SELCTION_MODE.DEFAULT) {
+      seclectionMode = SELCTION_MODE.DEFAULT
+      allSelectionModeButtons.forEach(button => {
+        button.classList.remove("active")
+      })
+      enableControl()
+      hideFrustumRectangle()
+    } else {
+      seclectionMode = newMode
+      htmlButton?.classList.add("active")
+      disableControl()
+    }
+  }
+
+  //#region Html event
+  fustumModeButton.onclick = () => {
+    onChangeMode(SELCTION_MODE.FUSTUM, fustumModeButton)
+  }
+  //#endregion
 }
 
 main();
